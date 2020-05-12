f3741d2ce3dba8eb182a0c42550955a22cded44e
Simon <simonh72498@gmail.com>
2020-05-12 18:30:30 -0400

    got post functionality, no like functionality yet

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,7 +6,7 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
