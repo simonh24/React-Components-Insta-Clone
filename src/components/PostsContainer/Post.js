87ff6c6564e7be2137c82aab01a54f038f9fa0fd
Simon <simonh72498@gmail.com>
2020-05-12 18:54:48 -0400

    added liking

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -26,7 +26,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection likes={likes}/>
+      <LikeSection likes={likes} addLikes={addLikes}/>
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
